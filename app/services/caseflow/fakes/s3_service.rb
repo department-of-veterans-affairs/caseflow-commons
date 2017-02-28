98204dbf5a479dcd9288fe46ac909bc778f086f0
Mark Benjamin <mark@navahq.com>
2017-02-28 17:19:32 -0500

    Updating S3 service to take an optional bucket name

--- app/services/caseflow/fakes/s3_service.rb
+++ app/services/caseflow/fakes/s3_service.rb
@@ -3,19 +3,19 @@ module Caseflow
   class Fakes::S3Service
     cattr_accessor :files
 
-    def self.store_file(filename, content, _type = :content)
+    def self.store_file(filename, content, _type = :content, _bucket:)
       self.files ||= {}
       self.files[filename] = content
     end
 
-    def self.fetch_file(filename, dest_filepath)
+    def self.fetch_file(filename, dest_filepath, _bucket:)
       self.files ||= {}
       File.open(dest_filepath, "wb") do |f|
         f.write(files[filename])
       end
     end
 
-    def self.fetch_content(filename)
+    def self.fetch_content(filename, _bucket:)
       self.files ||= {}
       self.files[filename]
     end
--- app/services/caseflow/s3_service.rb
+++ app/services/caseflow/s3_service.rb
@@ -4,32 +4,33 @@ require "aws-sdk"
 # Thin interface to all things Amazon S3
 module Caseflow
   class S3Service
-    def self.store_file(filename, content_or_filepath, type = :content)
+    def self.store_file(filename, content_or_filepath, type = :content, bucket:)
       init!
 
       content = type == :content ? content_or_filepath : File.open(content_or_filepath, "rb")
 
-      @bucket.put_object(acl: "private",
+      @client.put_object(acl: "private",
+                         bucket: bucket || default_bucket,
                          key: filename,
                          body: content,
                          server_side_encryption: "AES256")
     end
 
-    def self.fetch_file(filename, dest_filepath)
+    def self.fetch_file(filename, dest_filepath, bucket:)
       init!
 
       @client.get_object(
         response_target: dest_filepath,
-        bucket: bucket_name,
+        bucket: bucket || default_bucket,
         key: filename
       )
     end
 
-    def self.fetch_content(filename)
+    def self.fetch_content(filename, bucket:)
       init!
 
       @client.get_object(
-        bucket: bucket_name,
+        bucket: bucket || default_bucket,
         key: filename
       ).body.read
     rescue Aws::S3::Errors::NoSuchKey
@@ -37,16 +38,14 @@ module Caseflow
     end
 
     def self.init!
-      return if @bucket
+      return if @client
 
       Aws.config.update(region: "us-gov-west-1")
 
       @client = Aws::S3::Client.new
-      @resource = Aws::S3::Resource.new(client: @client)
-      @bucket = @resource.bucket(bucket_name)
     end
 
-    def self.bucket_name
+    def self.default_bucket
       Rails.application.config.s3_bucket_name
     end
   end
