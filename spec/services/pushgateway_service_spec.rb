b901cde8ea35aaf32dfd78a7ff5ce462fbddf0f7
Robert A Alexander (USDS/OMB/EOP//VA) <Robert.Alexander@va.gov>
2018-07-06 12:26:41 -0400

    rubocop

--- spec/services/pushgateway_service_spec.rb
+++ spec/services/pushgateway_service_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-require 'fakeweb'
+require "fakeweb"
 
 describe Caseflow::PushgatewayService do
   context "mock tests" do
@@ -14,11 +14,14 @@ describe Caseflow::PushgatewayService do
     end
 
     context "service online and unhealthy" do
-      before {
+      before do
         FakeWeb.register_uri(
           :get, "http://127.0.0.1:9091/-/healthy",
-          :body => "Error",
-          :status => ["503", "Service Unavailable"])}
+          body: "Error",
+          status: ["503", "Service Unavailable"]
+        )
+      end
+
       after { FakeWeb.clean_registry }
 
       it "unhealthy when service generates non-2xx status" do
@@ -28,10 +31,13 @@ describe Caseflow::PushgatewayService do
     end
 
     context "service online and healthy" do
-      before {
+      before do
         FakeWeb.register_uri(
           :get, "http://127.0.0.1:9091/-/healthy",
-          :body => "OK")}
+          body: "OK"
+        )
+      end
+
       after { FakeWeb.clean_registry }
 
       it "healthy when service generates 2xx status" do
