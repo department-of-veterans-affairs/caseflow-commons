2234b6be7349e472fb8bb79c8ccc9cc8cf2449fc
Robert A Alexander (USDS/OMB/EOP//VA) <Robert.Alexander@va.gov>
2018-07-06 11:14:21 -0400

    fix

--- spec/services/pushgateway_service_spec.rb
+++ spec/services/pushgateway_service_spec.rb
@@ -4,7 +4,7 @@ require "pry"
 describe Caseflow::PushgatewayService do
   context "live tests" do
     it "fails when service is not running" do
-      expect(Caseflow::PushgatewayService.is_healthy?.to eq(false)
+      expect(Caseflow::PushgatewayService.is_healthy?.to eq(false))
     end
   end
 end
