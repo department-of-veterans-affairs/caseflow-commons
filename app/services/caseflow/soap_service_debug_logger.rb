450a72101352019845f54009b1dc23bb2a189843
Alec Kagebein <Alec.Kagebein@va.gov>
2023-10-10 14:56:32 -0500

    APPEALS-26838 temp change to test zeitwerk on efolder

--- app/services/caseflow/soap_service_debug_logger.rb
+++ app/services/caseflow/soap_service_debug_logger.rb
@@ -1,14 +1,17 @@
 # may be used with VBMS or VVA clients, pass instance to 'log' or 'logger' param (see client docs)
 
-class SoapServiceDebugLogger
-  def log(event, data)
-    case event
-    when :request
-      puts "#{Time.zone.now} SOAP Request"
-      pp data
-    when :response
-      puts "#{Time.zone.now} SOAP Response"
-      pp data
+module Caseflow
+  class SoapServiceDebugLogger
+    def log(event, data)
+      case event
+      when :request
+        puts "#{Time.zone.now} SOAP Request"
+        pp data
+      when :response
+        puts "#{Time.zone.now} SOAP Response"
+        pp data
+      end
     end
   end
 end
+  
