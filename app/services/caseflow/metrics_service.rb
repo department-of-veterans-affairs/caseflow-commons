7b6e58f28d831fa55c06d725f0a21abd1de7b37a
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2023-11-29 15:18:02 -0500

    Add Caseflow check.

--- app/services/caseflow/metrics_service.rb
+++ app/services/caseflow/metrics_service.rb
@@ -109,7 +109,8 @@ module Caseflow
     # :reek:ControlParameter
     def self.store_record_metric(uuid, params, caller)
       return nil unless FeatureToggle.enabled?(:metrics_monitoring, user: RequestStore[:current_user])
-  #add check for if caseflow
+      #TODO: App name check requires validation. APP_NAME is used differently throughout the application.
+      return nil unless params[:product] == Caseflow::MetricAttributes::PRODUCT_TYPES[caseflow:]
       name = "caseflow.server.metric.#{params[:name]&.downcase&.gsub(/::/, '.')}"
       params = {
         uuid: uuid,
