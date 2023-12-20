67221f8609b0e98941534bdec10a612bd8469ec4
Noelle Adkin <98478937+NoelleAd@users.noreply.github.com>
2023-12-20 10:42:17 -0500

    comment out Dynatrace service for testing

--- app/services/caseflow/dynatrace_service.rb
+++ app/services/caseflow/dynatrace_service.rb
@@ -2,66 +2,66 @@
 
 class ExternalApi::DynatraceService
 
-        BASE_URL = ENV["DYNATRACE_URL"]
-        API_TOKEN = ENV["DYNATRACE_TOKEN"]
+  #      BASE_URL = ENV["DYNATRACE_URL"]
+  #      API_TOKEN = ENV["DYNATRACE_TOKEN"]
   
   class << self
   
   def  increment(stat_name, tags: tags, by: by)
     #stuff
     # build request
-    request = HTTPI::Request.new(BASE_URL)
-    request.open_timeout = 300
-    request.read_timeout = 300
-    request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
+    # request = HTTPI::Request.new(BASE_URL)
+    # request.open_timeout = 300
+    # request.read_timeout = 300
+    # request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
 
     # build body
-    request.body = render json: {
-      displayName: stat_name,
-      description: "",
-      unit: "Unspecified",
-      tags: tags,
-      }
+    # request.body = render json: {
+    #   displayName: stat_name,
+    #   description: "",
+    #   unit: "Unspecified",
+    #   tags: tags,
+    #   }
 
-    HTTPI.post(request)
+    # HTTPI.post(request)
   end
 
   def gauge(stat_name, metric_value, tags: tags)
     #stuff
     # build request
-    request = HTTPI::Request.new(BASE_URL)
-    request.open_timeout = 300
-    request.read_timeout = 300
-    request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
+    # request = HTTPI::Request.new(BASE_URL)
+    # request.open_timeout = 300
+    # request.read_timeout = 300
+    # request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
 
-    # build body
-    request.body = render json: {
-      displayName: stat_name,
-      description: "",
-      unit: "Unspecified",
-      tags: tags,
-      }
+    # # build body
+    # request.body = render json: {
+    #   displayName: stat_name,
+    #   description: "",
+    #   unit: "Unspecified",
+    #   tags: tags,
+    #   }
 
-    HTTPI.post(request)
+    # HTTPI.post(request)
   end
 
   def histogram(stat_name, metric_value, tags: tags)
     #stuff
     # build request
-    request = HTTPI::Request.new(BASE_URL)
-    request.open_timeout = 300
-    request.read_timeout = 300
-    request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
+    # request = HTTPI::Request.new(BASE_URL)
+    # request.open_timeout = 300
+    # request.read_timeout = 300
+    # request.auth.ssl.ca_cert_file = ENV["SSL_CERT_FILE"]
 
-    # build body
-    request.body = render json: {
-      displayName: stat_name,
-      description: "",
-      unit: "Unspecified",
-      tags: tags,
-      }
+    # # build body
+    # request.body = render json: {
+    #   displayName: stat_name,
+    #   description: "",
+    #   unit: "Unspecified",
+    #   tags: tags,
+    #   }
 
-    HTTPI.post(request)
+    # HTTPI.post(request)
   end
 end
   
